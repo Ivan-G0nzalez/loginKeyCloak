386b39380982a3b995293d4f676e1629c2389fa9
RenanDev3 <renan_cb@hotmail.com>
2023-03-03 14:48:31 -0300

    integrate login with microsoft service

--- keycloak.tsx
+++ keycloak.tsx
@@ -1,9 +1,9 @@
 import Keycloak from "keycloak-js";
 
 const keycloakConfig = {
-  url: "http://localhost:8080/",
-  realm: "keycloak-react-auth",
-  clientId: "react-cl",
+  url: "http://localhost",
+  realm: "internship",
+  clientId: "react_cl",
 };
 
 const keycloak = new Keycloak(keycloakConfig);
